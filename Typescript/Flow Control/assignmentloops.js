818a4b8e1a954239897910e392e8946e50468d27
Prakash-Balu <prakash.engg89@gmail.com>
2026-05-14 11:18:38 +0530

    Assignment loops changes

--- Typescript/Flow Control/assignmentloops.js
+++ Typescript/Flow Control/assignmentloops.js
@@ -15,3 +15,20 @@ if (input !== null) {
         console.log("Please enter a valid number.");
     }
 }
+// Prompt user for input
+const input2 = prompt("Enter a number:");
+if (input2 !== null) {
+    const num = parseInt(input2);
+    if (!isNaN(num)) {
+        let i = 1;
+        while (i <= num) {
+            if (i % 5 !== 0) {
+                console.log(i);
+            }
+            i++;
+        }
+    }
+    else {
+        console.log("Please enter a valid number.");
+    }
+}
